a4bb43f95b3416c7628832cee7e46f4b2f330e05
zhehuan.quan <zhehuan.quan@etu.u-pec.fr>
2020-11-08 22:24:12 +0100

    maj page comparaison

--- src/quancity/ui/Analyse_comparison.java
+++ src/quancity/ui/Analyse_comparison.java
@@ -12,6 +12,7 @@ import javax.swing.JTextField;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.eltima.components.ui.DatePicker;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -20,6 +21,8 @@ import quancity.client.common.ApiEnum;
 import quancity.client.common.SendPackage;
 
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.util.Calendar;
@@ -30,12 +33,29 @@ import java.awt.event.ActionEvent;
 import javax.swing.SwingConstants;
 import java.awt.Font;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Date;
+import java.util.Locale;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+import java.awt.SystemColor;
+
 public class Analyse_comparison{
 
 	public JFrame frame;
 	Client client;
 	private int cityID;
 	private Date date;
+	private JTextField txtDate;
+	private JTextField txtDate_1;
 	
 	/**
 	 * Create the application.
@@ -63,11 +83,6 @@ public class Analyse_comparison{
 		frame.getContentPane().add(panel);
 		panel.setLayout(new GridLayout(0, 2, 0, 8));
 		
-		JLabel lblNewLabel = new JLabel("<html><body><p align=\"center\">Compare with : <br/>(type date format : yyyy-mm-dd)</p></body></html>\" + \"(type date format : yyyy-mm-dd)</p></body></html>");
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		panel.add(lblNewLabel);
-		
 		JButton btnNewButton_3 = new JButton("Continue");
 		btnNewButton_3.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		
@@ -79,6 +94,19 @@ public class Analyse_comparison{
 			}
 		});
 		
+		txtDate = new JTextField();
+		txtDate.setFont(new Font("Tahoma", Font.ITALIC, 16));
+		txtDate.setHorizontalAlignment(SwingConstants.CENTER);
+		txtDate.setText("Date 1");
+		panel.add(txtDate);
+		txtDate.setColumns(10);
+		
+		txtDate_1 = new JTextField();
+		txtDate_1.setFont(new Font("Tahoma", Font.ITALIC, 16));
+		txtDate_1.setHorizontalAlignment(SwingConstants.CENTER);
+		txtDate_1.setText("Date 2");
+		panel.add(txtDate_1);
+		txtDate_1.setColumns(10);
 		JLabel label = new JLabel("");
 		panel.add(label);
 		
